135c739fb898e7c64ee632fb64a8ffa5e5638566
saravanan <saravanan123velu@gmail.com>
2026-02-22 11:44:30 +0530

    fixed typo errors

--- backend/src/controllers/MasterCategory/getGetMasterCategoryController.js
+++ backend/src/controllers/MasterCategory/getGetMasterCategoryController.js
@@ -6,7 +6,7 @@ const MasterCategoryService = require("../../services/MasterCategory.service");
 async function getMasterCategoryController(req,res,next){
      try{
            const masterCategoryServiceObj = new MasterCategoryService();
-           const masterCategoryId = req.body.masterCategoryId;
+           const masterCategoryId = req.query.masterCategoryId;
            if(!masterCategoryId){
                 throw new AppError(500,"MasterCategoryID is required!!!");
            }
